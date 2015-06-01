08b9f24d52468f75959bef9a7e95f382252d9b81
ptroyanov <paul.troyanov@gmail.com>
2015-06-01 17:51:17 +0300

    bootstrap_add

--- createUser.php
+++ createUser.php
@@ -5,13 +5,15 @@
 		createUser($_POST['login'], $_POST['email'], $_POST['password'], getConnect());
 	}
 ?>
-	<center>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+	<center style='margin-top:100px'>
 	<form method='POST'>
 
 	<p>Login:<input name='login'></p>
 	<p>Email:<input name='email'></p>
 	<p>Password:<input type='password' name='password'></p>
-	<p><input type='submit' value='Create user'></p>
+	<p><input type='submit' value='Create user' class="btn btn-primary"></p>
 	</form>
 	<?php include('userList.php'); ?>
 	</center>
--- editUser.php
+++ editUser.php
@@ -9,7 +9,7 @@
 		$_POST['email'] = $user['email'];
 
 	if(isset($_POST['sub']) && strlen($_POST['password']) == 0)
-		$_POST['password'] == $user['password'];
+		$_POST['password'] = $user['password'];
 
 	if(!empty($_POST) && strlen($_POST['login']) > 0){
 		updateUser($_POST['login'], $_POST['email'], $_POST['password'], $_GET['email'], getConnect());
@@ -17,13 +17,15 @@
 		exit();
 	}
 ?>
-	<center>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<center style='margin-top:100px'>
+
 	<form method='POST'>
 
 	<?="<p>Login:<input name='login' placeholder=".$user['name']."></p>"?>
 	<?="<p>Email:<input name='email' placeholder=".$user['email']."></p>"?>
 	<?="<p>Password:<input type='password' name='password' placeholder=".$user['password']."></p>"?>
-	<p><input name='sub' type='submit' value='Edit'></p>
+	<p><input name='sub' type='submit' value='Edit' class="btn btn-primary"></p>
 
 	</form>
 	</center>
